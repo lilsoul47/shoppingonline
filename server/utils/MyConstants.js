dafc8eb2131756c28acc5af83117e4abcc5bdcb0
lilsoul47 <149313599+lilsoul47@users.noreply.github.com>
2024-07-16 10:51:25 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clusterkv.tsg2ckk.mongodb.net',
+    DB_USER: 'lilsoul',
+    DB_PASS: 'k10101010',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'caigicungduoc',
+    JWT_EXPIRES: '36000000000000000000000000', // in milliseconds
+    EMAIL_USER: 'khoivo9933@gmail.com', // gmail service
+    EMAIL_PASS: 'cveg jkoq unxv cxsm'
+  };
+  module.exports = MyConstants;
